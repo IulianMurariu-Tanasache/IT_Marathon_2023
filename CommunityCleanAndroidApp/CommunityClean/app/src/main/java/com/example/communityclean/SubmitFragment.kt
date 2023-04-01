287ad7a201cea769d3a348ad3aa609f27224f79e
Alexandru Toma <toma.alex.xd@gmail.com>
2023-04-01 20:35:56 +0300

    finished submit fragment

--- CommunityCleanAndroidApp/CommunityClean/app/src/main/java/com/example/communityclean/SubmitFragment.kt
+++ CommunityCleanAndroidApp/CommunityClean/app/src/main/java/com/example/communityclean/SubmitFragment.kt
@@ -9,6 +9,9 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.communityclean.databinding.FragmentSubmitBinding
+import com.example.communityclean.dtos.ReportDto
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 
 
 class SubmitFragment : Fragment() {
@@ -36,7 +39,17 @@ class SubmitFragment : Fragment() {
             returnToHome()
         }
 
-        binding.textViewReport.text = args.reportObject
+        val report = Json.decodeFromString<ReportDto>(args.reportObject)
+        binding.textViewReport.text = """
+            |Category: ${report.category}
+            |Comments: ${report.comments}
+            |Contacts:
+            |   First Name: ${report.contacts?.firstName}
+            |   Last Name: ${report.contacts?.lastName}
+            |   Email: ${report.contacts?.email}
+            |   Phone: ${report.contacts?.tel}
+            |GeoLocation: ${report.geoTagging.latitude}, ${report.geoTagging.longitude} 
+        """.trimMargin()
         binding.textViewReport.movementMethod = ScrollingMovementMethod()
     }
 
